669089064cb187414dd69ad2ac84bff1bfabdb19
Ann Sarviro <annka_nuha@ai.ua>
2023-05-22 11:50:00 +0300

    fix: fix bugs in ChooseRouterSlider

--- src/widgets/ChooseRoutSlider/ChooseRoutSlider.tsx
+++ src/widgets/ChooseRoutSlider/ChooseRoutSlider.tsx
@@ -4,6 +4,11 @@ import { Navigation } from 'swiper';
 import { SliderObject } from '../../pages/api/SliderObject';
 import ChooseRoutCard from '@/entities/ChooseRoutCard/ui/ChooseRoutCard';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const ChooseRoutSlider = (data: SliderObject) => {
   let sliderArr = Object.values(data).map(el => el);
 
