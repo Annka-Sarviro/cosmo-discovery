7feb2b01cc272cab1ca13a765dd6819e7db03b2c
Ann Sarviro <annka_nuha@ai.ua>
2023-05-24 15:26:29 +0300

    updaate: add loader in ChooseRoutcard

--- src/entities/ChooseRoutCard/ui/ChooseRoutCard.tsx
+++ src/entities/ChooseRoutCard/ui/ChooseRoutCard.tsx
@@ -4,16 +4,30 @@ import Link from 'next/link';
 import Slide from '../model/Slide';
 import LeftDots from '../../../../public/leftDots.svg';
 import RightDots from '../../../../public/rightDots.svg';
+import Spinner from '../../../../public/spinner.svg';
+
+import { useState } from 'react';
+// import Loading from '@/app/Loading';
 
 const ChooseRoutCard = (data: Slide) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+  };
   return (
-    <li>
+    <li className="relative">
+      {isLoading && (
+        <div className="absolute left-[50%] right-0 top-[44%] ">
+          <Spinner />
+        </div>
+      )}
       <Image
         src={data.src}
         alt={data.alt}
         height={522}
         width={836}
         className="mx-auto max-md:mb-6 rounded-rad16 shadow-slideShadow"
+        onLoadingComplete={handleLoadingComplete}
       />
       <div className="flex justify-center items-center ">
         <LeftDots className="max-[570px]:invisible" />
