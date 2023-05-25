d77d5d9fc27c5d287b43a72e33c6d8745cb0e54b
Ann Sarviro <annka_nuha@ai.ua>
2023-05-25 12:41:12 +0300

    fix: fix bugs eroor border color in formField

--- src/shared/ui/FormField/Field.tsx
+++ src/shared/ui/FormField/Field.tsx
@@ -21,7 +21,7 @@ const Field: FC<FieldProps> = ({ data, reg, errors }) => {
       </label>
       <input
         id="formField"
-        className={`relative py-3 px-9 md:py-4 md:px-16 rounded-rad32 mb-16 border-2 border-transparent last:mb-0 placeholder:text-gray-600 text-darkBlue-90 bg-purple-10 max-xs:min-w-[290px] min-w-[calc(100vw_-_12rem)] md:min-w-[calc(100vw_-_35rem)] outline-[none] focus:outline-transparent focus:border-transparent max-md:text-base ${
+        className={`relative py-3 px-9 md:py-4 md:px-16 rounded-rad32 mb-16 border-2 last:mb-0 placeholder:text-gray-600 text-darkBlue-90 bg-purple-10 max-xs:min-w-[290px] min-w-[calc(100vw_-_12rem)] md:min-w-[calc(100vw_-_35rem)] outline-[none] focus:outline-transparent focus:border-transparent max-md:text-base ${
           errors[name]
             ? 'border-purple-80 border-2 border-style:solid focus:outline-purple-80 focus:border-purple-80'
             : 'border-transparent'
