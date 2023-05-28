2c9509e7af0a72c855a609f4c966c068a14f77ae
Ann Sarviro <annka_nuha@ai.ua>
2023-05-29 01:06:07 +0300

    feat: add datoCms intagration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,50 +2,43 @@ import { InferGetStaticPropsType } from 'next';
 import { request } from '@/lib/datocms';
 
 import Home from './home/Home';
-// import { SliderObject } from './api/SliderObject';
+import { SliderObject } from './api/SliderObject';
 
 const HOMEPAGE_QUERY = `query HomePage {
-  rout {
-    id, img { responsiveImage(imgixParams: { fit: crop, w: 300, h: 300, auto: format }) {
-        sizes
+  
+  allRouts {
+    id
+    title
+    _status
+    img {
+      id
+      url
+      responsiveImage {
         src
-        width
         height
-        alt
-        title
+        width
         base64
-      }}, title,  description,alt
+        alt
+        srcSet
+        sizes
+        webpSrcSet
+        title
+        bgColor
+        aspectRatio
+      }
+    }
   }
+
 }`;
 export async function getStaticProps() {
-  const data = await request({
+  const data: SliderObject = await request({
     query: HOMEPAGE_QUERY,
   });
   return {
-    props: { data },
+    props: data,
   };
 }
 
-export default function HomePage({ data }: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(data);
-  return <Home {...data} />;
+export default function HomePage(data: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <Home {...data.allRouts} />;
 }
-
-// export const getStaticProps = async () => {
-//   try {
-//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/getSlide`);
-//     const data: SliderObject = await res.json();
-
-//     return {
-//       props: {
-//         data,
-//       },
-//     };
-//   } catch {
-//     return {
-//       props: {
-//         data: null,
-//       },
-//     };
-//   }
-// };
