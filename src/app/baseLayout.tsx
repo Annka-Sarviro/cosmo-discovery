2efdcb428629d0f67756caaa78bd12252ab13aa9
Ann Sarviro <Annka_nuha@i.ua>
2023-07-21 11:23:44 +0300

    feat:add versel statistic

--- src/app/baseLayout.tsx
+++ src/app/baseLayout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import { FC, PropsWithChildren } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 import Header from '@/widgets/Header/Header';
 import Footer from '@/widgets/Footer/Footer';
@@ -12,7 +13,9 @@ const Layout: FC<PropsWithChildren<IMeta>> = ({ children, title, description }) 
   return (
     <Meta title={title} description={description}>
       <Header />
-      <main style={inter.style}>{children}</main>
+      <main style={inter.style}>{children}
+      <Analytics />
+      </main>
       <Footer />
     </Meta>
   );
